26395bb64d057d5ff85a4b99707bffd9de5ff99b
Jonathan Ohayon <me@jonathano.com>
2019-05-16 09:42:26 +0300

    Added rank, eigenvalues, Frobenius norm calculations as extensions to Matrix<Double>

--- src/main/kotlin/kontrol/Extensions.kt
+++ src/main/kotlin/kontrol/Extensions.kt
@@ -2,12 +2,26 @@ package kontrol
 
 import koma.extensions.mapIndexed
 import koma.matrix.Matrix
+import koma.matrix.ejml.EJMLMatrix
+import org.ejml.data.Complex_F64
+import kotlin.math.sqrt
 
 /**
- * Calculates the Moore-Penrose inverse (pseudoinverse) of a double matrix.
+ * Calculates the rank of a double matrix using its SVD
  */
-fun Matrix<Double>.pinv(): Matrix<Double> {
-  val (U, S, V) = this.SVD()
-  val pinvS = S.mapIndexed { i, j, t -> if (i == j && t != 0.0) 1.0 / t else t }
-  return V * pinvS * U.T
+fun Matrix<Double>.rank(): Int {
+  val (_, S, _) = this.SVD()
+  return S.diag().toIterable().filter { it != 0.0 }.size
 }
+
+/**
+ * Returns the eigenvalues of a given matrix, using the EJML backend function
+ */
+fun Matrix<Double>.eig(): MutableList<Complex_F64>? {
+  return (this as? EJMLMatrix)?.getBaseMatrix()?.eig()?.eigenvalues
+}
+
+/**
+ * Calculates the Frobenius norm of a double matrix
+ */
+fun Matrix<Double>.norm(): Double = sqrt((this * this.transpose()).trace())
